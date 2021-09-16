ac3fcce80c7af7d10654567956cf21920ec1509d
Zimo Xiao <xiaozimo@zuggr.com>
2021-09-16 22:53:36 +0800

    Update CyberGISCompute.py

--- cybergis_compute_client/CyberGISCompute.py
+++ cybergis_compute_client/CyberGISCompute.py
@@ -84,6 +84,9 @@ class CyberGISCompute:
             ])
 
         if self.isJupyter:
+            if len(data) == 0:
+                print('empty')
+                return
             display(HTML(tabulate(data, headers, numalign='left', stralign='left', colalign=('left', 'left'), tablefmt='html').replace('<td>', '<td style="text-align:left">').replace('<th>', '<th style="text-align:left">')))
         else:
             print(tabulate(data, headers, tablefmt="presto"))
