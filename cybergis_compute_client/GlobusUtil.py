ba8579cf722e7d2f01ac26524a9b3373880b58b3
Zimo Xiao <34933627+zimo-xiao@users.noreply.github.com>
2022-01-07 17:07:33 +0800

    add globus util

---
+++ cybergis_compute_client/GlobusUtil.py
@@ -0,0 +1,19 @@
+class GlobusUtil:
+    def __init__(self, compute):
+        self.compute = compute
+
+    def download(self, endpoint, path, hpc):
+        self.compute.login()
+        return self.compute.client.request('GET', '/globus-util/jupyter/download', {
+            'jupyterhubApiToken': self.compute.jupyterhubApiToken,
+            'to': '{}:{}'.format(endpoint, path),
+            'hpc': hpc
+        })
+    
+    def upload(self, endpoint, path, hpc):
+        self.compute.login()
+        return self.compute.client.request('GET', '/globus-util/jupyter/upload', {
+            'jupyterhubApiToken': self.compute.jupyterhubApiToken,
+            'from': '{}:{}'.format(endpoint, path),
+            'hpc': hpc
+        })
